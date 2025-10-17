c68a64d87370627c96033d7b3f86034744dbee52
Leonid Gurianov <leonid.11521@gmail.com>
2025-10-17 23:06:59 +0300

    changes in processing_rpn

--- src/processing_rpn.py
+++ src/processing_rpn.py
@@ -12,12 +12,12 @@ def processing_rpn(expression: str) -> str:
             Возвращает строку с рузультатом если выражение написанно верно, в противном случае возвращает пустую строку.
     """
 
-    expression = expression.replace("~~", "")
+    expression = re.sub(r'~ {0,}~', r'',expression)  # заменяет выражение два подряд идущих унарных минуса пустой строкой
     expression = re.sub(r'~([0-9]+(?:\.[0-9]+)?)', r'(~\1)',expression)  # заменяте выражения вида ~n на (~n)
     expression = re.sub(r'([0-9]+(?:\.[0-9]+)?) {0,}\^ {0,}([0-9]+(?:\.[0-9]+)?)', r'(\1^\2)',expression)  # заменяет выражения вида n^q а (n^q)
-    expression = expression.replace("-", "+~")
-    expression = expression.replace("$", "")
-    tokens = re.findall(r"[0-9]+(?:\.[0-9]+)?|//|[+*/^%$-]|[()]|\S+?", expression)
+    expression = re.sub(r'-', r'+~', expression) # заменяет минус на плюс с унарным минусом
+    expression = re.sub(r'$', r'', expression) # удоляет из строки унарный плюс
+    tokens = re.findall(r"[0-9]+(?:\.[0-9]+)?|//|[-+*/^%$~]|[()]", expression) # возвращает массив со всеми числами, скобками, операциями
 
     try:
         result: str = count_rpn(rpn_parse(tokens))
